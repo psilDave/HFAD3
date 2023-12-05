44e0b5648dfe6e7cef04c71496bab29a5eca4d01
Davi Pereira <davipsilva7@gmail.com>
2023-12-05 19:28:23 -0300

    Initializing my branch - HFAD3

---
+++ Beer Adviser/app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.hfad.beeradviser
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val brands = findViewById<TextView>(R.id.brands)
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce { str, item -> str + "\n" + item }
+            brands.text = beers
+        }
+    }
+
+    private fun getBeers(color: String): List<String> {
+        return when (color) {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Ambar" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
+    }
+}
